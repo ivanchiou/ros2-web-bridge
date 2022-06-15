4f422092f417ea6695f13f0fa34624252d4debce
ivanchiou <wyattkidd@gmail.com>
2022-06-15 17:27:04 +0800

    1. added python websocket publisher/subscriber 2. k8s yaml files draft

---
+++ python/ros_ws_publisher.py
@@ -0,0 +1,21 @@
+import roslibpy
+import time
+import os
+
+WS_HOST = os.getenv('WS_HOST', 'localhost')
+WS_PORT = os.getenv('WS_PORT', '9090')
+TOPIC_NAME = os.getenv('TOPIC_NAME', 'example_topic')
+TIME_PERIOD = os.getenv('TIME_PERIOD', '1')
+DATA_MSG = os.getenv('DATA_MSG', 'Hello World!')
+
+client = roslibpy.Ros(host=WS_HOST, port=WS_PORT)
+client.run()
+talker = roslibpy.Topic(client, '/'+TOPIC_NAME, 'std_msgs/String')
+
+while client.is_connected:
+    talker.publish(roslibpy.Message({'data': DATA_MSG}))
+    print('Sending message...')
+    time.sleep(TIME_PERIOD)
+
+talker.unadvertise()
+client.terminate()
---
+++ python/ros_ws_subscriber.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import roslibpy
+import os
+
+WS_HOST = os.getenv('WS_HOST', "localhost")
+WS_PORT = os.getenv('WS_PORT', "9090")
+TOPIC_NAME = os.getenv('TOPIC_NAME', "example_topic")
+
+client = roslibpy.Ros(host=WS_HOST, port=WS_PORT)
+client.run()
+
+listener = roslibpy.Topic(client, '/'+TOPIC_NAME, 'std_msgs/String')
+listener.subscribe(lambda message: print('Heard publisher: ' + message['data']))
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    client.terminate()

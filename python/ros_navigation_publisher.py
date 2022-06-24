62ea6867415c423fae94a7fecf6b2e85a0e15909
ivanchiou <wyattkidd@gmail.com>
2022-06-24 18:19:56 +0800

    auto added point location in loop for navigation publisher

--- python/ros_navigation_publisher.py
+++ python/ros_navigation_publisher.py
@@ -2,13 +2,10 @@
 import roslibpy
 import json
 import time
-import sys
 import os
-from datetime import datetime
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PointStamped
-from std_msgs.msg import String
 
 WS_HOST = os.getenv('WS_HOST', 'localhost')
 WS_PORT = os.getenv('WS_PORT', '9090')
@@ -27,47 +24,36 @@ class NavigationPublisher(Node):
         pub = roslibpy.Topic(client, '/'+TOPIC_NAME_STRING, MESSAGE_FORMAT_STRING)    
         #pub.subscribe(lambda message: print('Heard publisher: {}'.format(message['data'])))
 
-        x = -20.0
-        y = -20.0
-
-        msg = PointStamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "navigation"
-        msg.point.x = x
-        msg.point.y = y
-        msg.point.z = 0.
-
-        msg_dict = { 
-            'message': {
-                'header': {
-                    'seq': 377284,
-                    'stamp': {
-                        'sec': msg.header.stamp.sec,
-                        'nsec': msg.header.stamp.nanosec,
-                    },
-                    'frame_id': "navigation"
-                },
-                'point': {
-                    'x': x,
-                    'y': y,
-                    'z': 0.
-                }
-            },
-            'message_type': MESSAGE_FORMAT,
-            'topic_name': TOPIC_NAME
-        }
-
-        '''
-        self.subscription = self.create_subscription(
-            PointStamped,
-            '/'+TOPIC_NAME_STRING,
-            self.on_message,
-            10)
-        self.subscription        
-        '''
-
         i = 1
         while client.is_connected:
+            x = 0 + i
+            y = 0 + i
+
+            msg = PointStamped()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "navigation"
+            msg.point.x = x
+            msg.point.y = y
+            msg.point.z = 0.
+            msg_dict = { 
+                'message': {
+                    'header': {
+                        'seq': 377284,
+                        'stamp': {
+                            'sec': msg.header.stamp.sec,
+                            'nsec': msg.header.stamp.nanosec,
+                        },
+                        'frame_id': "navigation"
+                    },
+                    'point': {
+                        'x': msg.point.x,
+                        'y': msg.point.y,
+                        'z': msg.point.z
+                    }
+                },
+                'message_type': MESSAGE_FORMAT,
+                'topic_name': TOPIC_NAME
+            }
             pub.publish(roslibpy.Message({'data': json.dumps(msg_dict)}))
             print('Sending message...'+str(i)+'...'+json.dumps(msg_dict))
             i = i + 1

6313ef2792937352ccf636fbd4bae7ee5b19daef
ivanchiou <wyattkidd@gmail.com>
2022-06-24 16:38:28 +0800

    added geometry_msgs.msg PoseStamped sample code for rviz

---
+++ python/ros_navigation_publisher.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+import roslibpy
+import json
+import time
+import sys
+import os
+from datetime import datetime
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import String
+
+WS_HOST = os.getenv('WS_HOST', 'localhost')
+WS_PORT = os.getenv('WS_PORT', '9090')
+TOPIC_NAME = os.getenv('TOPIC_NAME', 'clicked_point')
+TOPIC_NAME_STRING = os.getenv('TOPIC_NAME_STRING', 'clicked_point_string')
+MESSAGE_FORMAT = os.getenv('MESSAGE_FORMAT', 'geometry_msgs/PoseStamped')
+MESSAGE_FORMAT_STRING = os.getenv('MESSAGE_FORMAT_STRING', 'std_msgs/String')
+TIME_PERIOD = os.getenv('TIME_PERIOD', '60')
+
+class NavigationPublisher(Node):
+    def __init__(self):
+        super().__init__("navigation_node")
+        client = roslibpy.Ros(host=WS_HOST, port=int(WS_PORT))
+        client.run()
+
+        pub = roslibpy.Topic(client, '/'+TOPIC_NAME_STRING, MESSAGE_FORMAT_STRING, queue_size=10)    
+        pub.subscribe(lambda message: print('Heard publisher: {}'.format(message['data'])))
+
+        x = -20.0
+        y = -20.0
+
+        msg = PoseStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "navigation"
+        msg.pose.position.x = x
+        msg.pose.position.y = y
+        msg.pose.position.z = 0.
+
+        msg_dict = { 
+            'message': {
+                'header': {
+                    'seq': 377284,
+                    'stamp': {
+                        'sec': msg.header.stamp.sec,
+                        'nsec': msg.header.stamp.nanosec,
+                    },
+                    'frame_id': "navigation"
+                },
+                'pose': {
+                    'position': {
+                        'x': x,
+                        'y': y,
+                        'z': 0.
+                    }
+                }
+            },
+            'message_type': MESSAGE_FORMAT,
+            'topic_name': TOPIC_NAME
+        }
+
+        '''
+        self.subscription = self.create_subscription(
+            PoseStamped,
+            '/'+TOPIC_NAME_STRING,
+            self.on_message,
+            10)
+        self.subscription        
+        '''
+
+        i = 1
+        while client.is_connected:
+            pub.publish(roslibpy.Message({'data': json.dumps(msg_dict)}))
+            print('Sending message...'+str(i)+'...'+json.dumps(msg_dict))
+            i = i + 1
+            time.sleep(int(TIME_PERIOD))
+            #rate.sleep()
+    
+        pub.unadvertise()
+        client.terminate()
+
+if __name__ == '__main__':
+    rclpy.init()
+    node = NavigationPublisher()
+    try:
+        rclpy.spin(node)
+    except Exception as e:
+        print(e)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ python/ros_ws_navigation_subscriber_to_publisher.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import roslibpy
+import os
+from datetime import datetime
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+
+WS_HOST = os.getenv('WS_HOST', 'localhost')
+WS_PORT = os.getenv('WS_PORT', '9090')
+TOPIC_NAME_STRING = os.getenv('TOPIC_NAME_STRING', 'clicked_point_string')
+MESSAGE_FORMAT_STRING = os.getenv('MESSAGE_FORMAT_STRING', 'std_msgs/String')
+TIME_PERIOD = os.getenv('TIME_PERIOD', '60')
+
+class NavigationSubscriber(Node):
+    def __init__(self):
+        super().__init__("navigation_node")
+        client = roslibpy.Ros(host=WS_HOST, port=int(WS_PORT))
+        client.run()
+        pub = roslibpy.Topic(client, '/'+TOPIC_NAME_STRING, MESSAGE_FORMAT_STRING, queue_size=10)    
+        pub.subscribe(self.handle_message)
+
+    def on_message(self, msg):
+        print('Heard publisher: {}'.format(msg['data']))
+
+        data = msg['data']
+        message = data['message']
+
+        self.publisher = self.create_publisher(
+            PoseStamped,
+            '/'+data['topic_name'],
+            1)
+
+        msg = PoseStamped()
+        header = message['header']
+        msg.header.seq = header['seq']
+        msg.header.stamp = header['stamp']
+        msg.header.frame_id = header['frame_id']
+        pose = message['pose']
+        position = pose['position']
+        msg.pose.position.x = position['x']
+        msg.pose.position.y = position['y']
+        msg.pose.position.z = position['z']
+
+        self.publisher.publish(msg)
+
+if __name__ == '__main__':
+    rclpy.init()
+    node = NavigationSubscriber()
+    try:
+        rclpy.spin(node)
+    except Exception as e:
+        print(e)
+    node.destroy_node()
+    rclpy.shutdown()
